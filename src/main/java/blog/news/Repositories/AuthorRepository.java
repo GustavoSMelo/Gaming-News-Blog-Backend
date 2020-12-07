6152001fe9ecc386ed8026d9b96d6f0eaf6e42fa
GustavoSMelo <gsantos15569@gmail.com>
2020-12-06 23:55:43 -0300

    Changing extended class crudRepository to jpaRepository

--- src/main/java/blog/news/Repositories/AuthorRepository.java
+++ src/main/java/blog/news/Repositories/AuthorRepository.java
@@ -3,7 +3,6 @@ package blog.news.Repositories;
 import blog.news.Models.AuthorModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.RestController;
 
 @Repository
 public interface AuthorRepository extends JpaRepository<AuthorModel, Long> { }
