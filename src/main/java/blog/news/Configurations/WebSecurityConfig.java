589d497abca4ba09d5532201bd47e6204f79f037
GustavoSMelo <gsantos15569@gmail.com>
2020-12-06 23:54:13 -0300

    learning web security login in spring boot

--- src/main/java/blog/news/Configurations/WebSecurityConfig.java
+++ src/main/java/blog/news/Configurations/WebSecurityConfig.java
@@ -1,2 +1,31 @@
-package blog.news.Configurations;public class WebSecurityConfig {
+package blog.news.Configurations;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/author").hasRole("USER").and().httpBasic()
+                .and().authorizeRequests().antMatchers(HttpMethod.GET, "/author/login")
+                    .hasRole("USER").and().formLogin().loginProcessingUrl("/author/login");
+    }
+
+    @Autowired
+    protected void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        super.configure(auth);
+
+        auth.inMemoryAuthentication().withUser("gustavo").password("{noop}123456").roles("USER");
+    }
 }
